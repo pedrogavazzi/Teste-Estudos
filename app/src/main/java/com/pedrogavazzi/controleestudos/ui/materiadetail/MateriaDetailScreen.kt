23aa74fd4c0a2ba775ed77d90be2f44a2591a199
pedrogavazzi <78188088+pedrogavazzi@users.noreply.github.com>
2026-07-25 13:11:44 -0300

    Add files via upload

--- app/src/main/java/com/pedrogavazzi/controleestudos/ui/materiadetail/MateriaDetailScreen.kt
+++ app/src/main/java/com/pedrogavazzi/controleestudos/ui/materiadetail/MateriaDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.pedrogavazzi.controleestudos.ui.materiadetail
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
